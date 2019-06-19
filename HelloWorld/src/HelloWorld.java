b370305707dfd0f2494fbec106d0486f8d85e6ef
JunHyongPark <pjh12400@gmail.com>
2019-06-19 11:31:34 +0900

    19-06-19 AM:11:30 Master code

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Very Slow");
 		System.out.println("Hello Git");
-		System.out.println("³ªÁ» º¸³»Áà");
+		System.out.println("GitHub Good!");
 	}
 
 }
