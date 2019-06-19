418a6e0a355487fa36c55acddff754f3044c2c5d
JunHyongPark <pjh12400@gmail.com>
2019-06-19 11:13:22 +0900

    GOGO

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Very Slow");
 		System.out.println("Hello Git");
-		System.out.println("커몬 베이비");
+		System.out.println("나좀 보내줘");
 	}
 
 }
